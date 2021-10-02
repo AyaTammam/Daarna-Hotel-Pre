79da33f4adb91871dd9083c55cad7e540619bce9
AyaTammam <epiks763@gmail.com>
2021-10-02 10:22:06 +0300

    Modified The Project File

--- global/header.php
+++ global/header.php
@@ -43,3 +43,32 @@
             </div>
         </section>
         <!-- End Upper bar -->
+        <!-- Start Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">Salmya</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-info" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="nav-info">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">الرئيسية</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="i.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Work</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- End Navbar -->
--- index.php
+++ index.php
@@ -1,29 +1,87 @@
 <?php
     require 'global/header.php';
 ?>
-
-<section class="form">
+<!-- Start login -->
+<div class="log">
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <div>
-                    <!-- http://placehold.it/300/300 -->
-                    <img src="photos/login1.jpg" />
-                </div>
-            </div>
-            <div class="col">
-                <form class="login">
-                    <h4 class="text-center">Admin Login</h4>
-                    <input class="form-control form-control-lg" type="text" name="user" placeholder="UserNname" autocomplete="off" />
-                    <input class="form-control form-control-lg" type="password" name="pass" placeholder="Password" autocomplete="new-password" />
-                    <div class="d-grid gap-2">
-                        <button class="btn btn-primary d-grid gap-2" type="submit">Login</button>
-                    </div>
-                </form>
-            </div>
+        <div class="col-6 go">
+            <a class="btn btn-outline-dark btn-lg hvr-wobble-horizontal" role="button" href="login.php">
+                <i class="fa fa-sign-in-alt fa-fw m-auto d-block"></i>
+                LogIn
+            </a>
         </div>
     </div>
-</section>
+</div>
+<!-- End login -->
+<!-- Start Section -->
+<section class="first text-center">
+            <div class="overlay">
+                <div class="container">
+                    <h1>Section #1</h1>
+                    <div class="row">
+                        <div class="col">
+                            <h2>Section #1</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                        <div class="col">
+                            <h2>Section #1</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                        <div class="col">
+                            <h2>Section #1</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Section -->
+        <!-- Start Section -->
+        <section class="two text-center">
+            <div class="overlay">
+                <div class="container">
+                    <h2 class="h1 tw">Section #2</h2>
+                    <div class="row">
+                        <div class="col">
+                            <h2>Section #2</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                        <div class="col">
+                            <h2>Section #2</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                        <div class="col">
+                            <h2>Section #2</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Section -->
+        <!-- Start Section -->
+        <section class="three text-center">
+            <div class="overlay">
+                <div class="container">
+                    <h2 class="h1 th">Section #3</h2>
+                    <div class="row">
+                        <div class="col">
+                            <h2>Section #3</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                        <div class="col">
+                            <h2>Section #3</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                        <div class="col">
+                            <h2>Section #3</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odio molestias eveniet ut. Velit minus ex quos ea ab, modi, nam illum illo numquam sequi perferendis impedit ratione a. Natus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Section -->
 
 <?php
     require 'global/footer.php';
